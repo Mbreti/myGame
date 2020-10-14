86db68811334ef416f70bd5456590f6e64965ec4
John <61512014+Mbreti@users.noreply.github.com>
2020-10-14 11:25:51 +0500

    update

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 let game = {
+    gridSettings: {
+        squaresHorizontal: 100,
+        squaresVertical: 120,
+    },
     canvas: null,
     ctx: null,
     squat: null,
-    blocks: [],
     walls: null,
-    gridSettings: {
-        squareWidth: 100,
-        squareHeight: 120,
-    },
+    w: null,
+    h: null,
+    squareWidth: null,
+    squareHeight: null,
+
     init() {
         this.canvas = document.querySelector("#canvas");
         this.ctx = canvas.getContext("2d");
+        this.w = canvas.width
+        this.h = canvas.height
+        this.squareWidth = this.w / this.gridSettings.squaresHorizontal
+        this.squareHeight = this.h / this.gridSettings.squaresVertical
     },
 
     render() {
         this.squat.drawRect();
         this.squat.mouseEvent();
-        this.squat.collide(this.walls)
-        this.walls.createWalls();
-        this.renderGrid();
     },
 
     start() {
@@ -28,14 +33,10 @@ let game = {
 
     renderGrid() {
         this.ctx.beginPath()
-        const w = this.canvas.width
-        const h = this.canvas.height
-        const squareWidth = w / this.gridSettings.squareWidth
-        const squareHeight = h / this.gridSettings.squareHeight
         this.ctx.strokeStyle = '#bdbdbd'
-        this.ctx.lineWidth = 0.1
-        for (let x = squareWidth; x < w; x += squareWidth) this.ctx.strokeRect(x, 0, 0.1, h)
-        for (let y = squareHeight; y < h; y += squareHeight) this.ctx.strokeRect(0, y, w, 0.1)
+        this.ctx.lineWidth = 0.4
+        for (let x = this.squareWidth; x < this.w; x += this.squareWidth) this.ctx.strokeRect(x, 0, 0.1, this.h)
+        for (let y = this.squareHeight; y < this.h; y += this.squareHeight) this.ctx.strokeRect(0, y, this.w, 0.1)
         this.ctx.fill()
         this.ctx.closePath()
     }
@@ -49,14 +50,19 @@ game.squat = {
     x: 0,
     y: 0,
     start: null,
+
     edge(c, min, max) {
         return Math.max(min, Math.min(max, c));
     },
+
     drawRect(x = this.x, y = this.y) {
         game.ctx.fillStyle = "black";
         game.ctx.clearRect(0, 0, 500, 600);
         game.ctx.fillRect(x, y, this.width, this.height);
+        game.renderGrid()
+
     },
+
     mouseEvent() {
         game.canvas.onmousedown = (e) => {
             this.start = { x: e.offsetX, y: e.offsetY };
@@ -67,35 +73,18 @@ game.squat = {
                 );
             }
             game.canvas.onmouseup = (e) => {
-                this.x = this.edge(this.x + e.offsetX - this.start.x, 0, 500 - this.width);
-                this.y = this.edge(this.y + e.offsetY - this.start.y, 0, 600 - this.height);
+                const new_x = game.squareWidth * Math.round((this.x + e.offsetX - this.start.x) / game.squareWidth);
+                const new_y = game.squareHeight * Math.round((this.y + e.offsetY - this.start.y) / game.squareHeight);
+                this.x = this.edge(new_x, 0, 500 - 34);
+                this.y = this.edge(new_y, 0, 600 - 40);
+                this.drawRect(
+                    this.edge(this.x, 0, 500 - 34),
+                    this.edge(this.y, 0, 600 - 40)
+                );
                 game.canvas.onmousemove = null
             }
         }
-        return this.x, this.y
     },
-    collide(element) {
-        console.log(this.x, this.y,)
-        if (
-            this.x + this.width > element.x &&
-            this.x < element.x + element.width &&
-            this.y + this.height > element.y &&
-            this.y < element.y + element.height
-        ) {
-            console.log("ss")
-        }
-    }
-};
-
-game.walls = {
-    width: 100,
-    height: 20,
-    x: 50,
-    y: 10,
-    createWalls() {
-        game.ctx.fillStyle = "red";
-        game.ctx.fillRect(this.x, this.y, this.width, this.height)
-    }
 };
 
 window.addEventListener("DOMContentLoaded", () => {
