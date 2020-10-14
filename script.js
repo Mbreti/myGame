d1f7371698834d506d55218e53247ee01c149a09
John <61512014+Mbreti@users.noreply.github.com>
2020-10-14 14:58:39 +0500

    update

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ let game = {
     h: null,
     squareWidth: null,
     squareHeight: null,
+    gridCheckbox: null,
 
     init() {
         this.canvas = document.querySelector("#canvas");
         this.ctx = canvas.getContext("2d");
+
         this.w = canvas.width
         this.h = canvas.height
         this.squareWidth = this.w / this.gridSettings.squaresHorizontal
         this.squareHeight = this.h / this.gridSettings.squaresVertical
+
+        this.gridCheckbox = document.querySelector('input[id=toggleGrid]')
     },
 
     render() {
@@ -32,13 +36,16 @@ let game = {
     },
 
     renderGrid() {
-        this.ctx.beginPath()
-        this.ctx.strokeStyle = '#bdbdbd'
-        this.ctx.lineWidth = 0.4
-        for (let x = this.squareWidth; x < this.w; x += this.squareWidth) this.ctx.strokeRect(x, 0, 0.1, this.h)
-        for (let y = this.squareHeight; y < this.h; y += this.squareHeight) this.ctx.strokeRect(0, y, this.w, 0.1)
-        this.ctx.fill()
-        this.ctx.closePath()
+        if (this.gridCheckbox.checked) {
+            this.ctx.beginPath()
+            this.ctx.strokeStyle = '#bdbdbd'
+            this.ctx.lineWidth = 0.4
+            for (let x = this.squareWidth; x < this.w; x += this.squareWidth) this.ctx.strokeRect(x, 0, 0.1, this.h)
+            for (let y = this.squareHeight; y < this.h; y += this.squareHeight) this.ctx.strokeRect(0, y, this.w, 0.1)
+            this.ctx.fill()
+            this.ctx.closePath()
+        }
+        return false
     }
 
 };
@@ -58,9 +65,8 @@ game.squat = {
     drawRect(x = this.x, y = this.y) {
         game.ctx.fillStyle = "black";
         game.ctx.clearRect(0, 0, 500, 600);
-        game.ctx.fillRect(x, y, this.width, this.height);
         game.renderGrid()
-
+        game.ctx.fillRect(x, y, this.width, this.height);
     },
 
     mouseEvent() {
@@ -73,14 +79,19 @@ game.squat = {
                 );
             }
             game.canvas.onmouseup = (e) => {
-                const new_x = game.squareWidth * Math.round((this.x + e.offsetX - this.start.x) / game.squareWidth);
-                const new_y = game.squareHeight * Math.round((this.y + e.offsetY - this.start.y) / game.squareHeight);
-                this.x = this.edge(new_x, 0, 500 - 34);
-                this.y = this.edge(new_y, 0, 600 - 40);
-                this.drawRect(
-                    this.edge(this.x, 0, 500 - 34),
-                    this.edge(this.y, 0, 600 - 40)
-                );
+                if (game.gridCheckbox.checked) {
+                    const new_x = game.squareWidth * Math.round((this.x + e.offsetX - this.start.x) / game.squareWidth);
+                    const new_y = game.squareHeight * Math.round((this.y + e.offsetY - this.start.y) / game.squareHeight);
+                    this.x = this.edge(new_x, 0, 500 - this.width);
+                    this.y = this.edge(new_y, 0, 600 - this.height);
+                    this.drawRect(
+                        this.edge(this.x, 0, 500 - this.width),
+                        this.edge(this.y, 0, 600 - this.height)
+                    );
+                } else {
+                    this.x = this.edge(this.x + e.offsetX - this.start.x, 0, 500 - this.width);
+                    this.y = this.edge(this.y + e.offsetY - this.start.y, 0, 600 - this.height);
+                }
                 game.canvas.onmousemove = null
             }
         }
